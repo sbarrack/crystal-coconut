da102531d3f05740a3a2ba3f700229f96ad67152
Jacob <jacob@goldenneedham.com>
2020-05-08 19:33:58 -0700

    Added baseline for the enchantment

--- src/main/java/com/stephenbarrack/crystalCoconut/CrystalCoconut.java
+++ src/main/java/com/stephenbarrack/crystalCoconut/CrystalCoconut.java
@@ -1,5 +1,32 @@
 package com.stephenbarrack.crystalCoconut;
 
-public class CrystalCoconut {
-	
+import com.stephenbarrack.crystalCoconut.enchantments.Soulbound;
+import net.fabricmc.api.ModInitializer;
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.enchantment.EnchantmentTarget;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
+public class CrystalCoconut implements ModInitializer {
+
+    private static Enchantment SOULBOUND;
+
+    @Override
+    public void onInitialize() {
+        // This code runs as soon as Minecraft is in a mod-load-ready state.
+        // However, some things (like resources) may still be uninitialized.
+        // Proceed with mild caution.
+
+        System.out.println("Initalizing Soulbound");
+        SOULBOUND = Registry.register(
+                Registry.ENCHANTMENT,
+                new Identifier("Tweaks", "soulbound"),
+                new Soulbound(
+                        Enchantment.Rarity.VERY_RARE,
+                        EnchantmentTarget.BREAKABLE,
+                        EquipmentSlot.values()
+                )
+        );
+    }
 }
